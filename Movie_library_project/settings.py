565e5149b4046faf291084b91eaef4d26e8d9193
BuczynskiRafal <buczynski.rafal2@gmail.com>
2021-10-09 05:42:39 +0200

    All files for herku

--- Movie_library_project/settings.py
+++ Movie_library_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['movie-library-app-by-rafal.herokuapp.com', 'localhost:8000']
 
 
 # Application definition
